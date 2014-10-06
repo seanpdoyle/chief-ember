727f83faa73620e7e023aa5fd79bdafe22dc9c27
Sean Doyle <sean.p.doyle24@gmail.com>
2014-10-06 19:31:01 -0400

    Nearby controller reloads model from query params
    via http://emberjs.com/guides/routing/query-params/#toc_opting-into-a-full-transition

--- app/controllers/nearby.js
+++ app/controllers/nearby.js
@@ -1,17 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  reloadModel: function() {
-    var latitude = this.get('latitude');
-    var longitude = this.get('longitude');
-
-    if (latitude && longitude) {
-      var nearby = this.store.find('spot', {
-        latitude: this.get('latitude'),
-        longitude: this.get('longitude')
-      });
-
-      this.set('model', nearby);
-    }
-  }.observes('latitude', 'longitude')
+  queryParams: ['latitude', 'longitude'],
+  latitude: null,
+  longitude: null
 });
--- app/routes/nearby.js
+++ app/routes/nearby.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    latitude: { refreshModel: true },
+    longitude: { refreshModel: true }
+  },
   model: function() {
     return this.store.findQuery('spot', this.get('lastLocation'));
   }
