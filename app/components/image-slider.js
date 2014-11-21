c8a65a75c3d1ab5ef1e8cf6a878f5e0bb6db3743
Sean Doyle <sean.p.doyle24@gmail.com>
2014-11-21 18:43:37 -0500

    Improved Design for Desktop

--- app/components/image-slider.js
+++ app/components/image-slider.js
@@ -1,8 +1,22 @@
 import Ember from 'ember';
-/* global $ */
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    $(this.get('element')).owlCarousel();
+    Ember.run.scheduleOnce('afterRender', this, this.setupCarousel);
+  },
+
+  contentDidChange: function() {
+    Ember.run.scheduleOnce('afterRender', this, this.setupCarousel);
+  }.observes('content.[]'),
+
+  setupCarousel: function() {
+    var content = this.get('content');
+
+    this.$().owlCarousel({
+      autoPlay: false,
+      items: content.get('length'),
+      lazyLoad: true,
+      responsive: false
+    });
   }
 });
